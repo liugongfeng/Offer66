3b96aef9922cca7c38e32286ef145d175a553ad7
liugongfeng <liugong.feng@aliyun.com>
2019-04-14 18:02:00 +0800

    把数组排成最小的数

--- offer32.py
+++ offer32.py
@@ -6,8 +6,14 @@ class Solution:
         num = list(map(str, nums))
         num.sort(key = cmp_to_key(lambda b, a: ((a+b)<(b+a))-((a+b)>(b+a)) ))
         return ''.join(num).lstrip('0') or '0'
-    
 
+    def method2(self, nums):
+        def cmp(a,b):
+            return int(b+a) - int(a+b)
+
+        nums = list(map(str, nums))
+        nums.sort(key=cmp_to_key(cmp))
+        return ''.join(nums).lstrip('0') or '0'
 
 
 
